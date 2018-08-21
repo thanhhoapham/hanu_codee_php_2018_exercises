3693405e860d1a7b41ca25e651d894e462c8ab6e
daihiepvolam <hoangthitu1998@gmail.com>
2018-08-21 18:19:28 +0700

    Add exercise

---
+++ tuht/quizapp/back/list.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Questions List</title>
+	<h1>Question List</h1>
+</head>
+<style type="text/css">
+	h1{
+		text-align: center;
+		font-family: sans-serif;
+		color: #5656ce;
+		text-transform: uppercase;
+	}
+	table{
+		background-color: lightgrey;
+		margin: 0 auto;
+		text-align: center;
+		padding-top: 10px;
+		padding-bottom: 10px;
+	}
+    td{
+		padding-top: 10px;
+		padding-bottom: 10px;
+
+	}
+    tr:nth-child(even) {
+    background-color: white;
+    }
+    button{
+    	text-transform: uppercase;
+    	width: 100px;
+    	height: 40px;
+    	border: none;
+    	background-color: red;
+    }
+    button a{
+    	text-decoration: none;
+    	color:white;
+    	font-family: sans-serif;
+    	font-weight: bold;
+
+   
+    }
+</style>
+<body>
+	    
+	<table style="width: 1000px; font-family: sans-serif;">
+		
+		<tr>
+			<th>Question</th>
+			<th>Content</th>
+			<th>Answer</th>
+		</tr>
+		<tr>
+			<td>Question 1</td>
+			<td>abcxyz content question 1 is here</td>
+			<td>A</td>
+			<td><a href="">Edit</a></td>
+			<td><a href="">Delete</a></td>
+		</tr
+		<tr>
+			<td>Question 1</td>
+			<td>abcxyz content question 1 is here</td>
+			<td>A</td>
+			<td><a href="">Edit</a></td>
+			<td><a href="">Delete</a></td>
+		</tr>
+		<tr>
+			<td>Question 1</td>
+			<td>abcxyz content question 1 is here</td>
+			<td>A</td>
+			<td><a href="">Edit</a></td>
+			<td><a href="">Delete</a></td>
+		</tr>	
+		<tr>
+			<td>Question 1</td>
+			<td>abcxyz content question 1 is here</td>
+			<td>A</td>
+			<td><a href="">Edit</a></td>
+			<td><a href="">Delete</a></td>
+		</tr>
+		<tr>
+			<td>Question 1</td>
+			<td>abcxyz content question 1 is here</td>
+			<td>A</td>
+            <td><a href="">Edit</a></td>
+			<td><a href="">Delete</a></td>
+		</tr>
+		<tr>
+			<td>Question 1</td>
+			<td>abcxyz content question 1 is here</td>
+			<td>A</td>
+			<td><a href="">Edit</a></td>
+			<td><a href="">Delete</a></td>
+		</tr>
+		<tr>
+			<th colspan="4"><button style="align-self:center;"><a href="">add new</a></button><th>
+		</tr>
+	</table>
+</body>
+</html>
