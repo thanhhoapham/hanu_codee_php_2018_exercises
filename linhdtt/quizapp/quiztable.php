942be11910149e907a755f49270e89faf0de0c76
Linh Anh <annielin512@gmail.com>
2018-08-21 18:08:03 +0700

    Edit me!

---
+++ linhdtt/quizapp/quiztable.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Quiz App Table </title>
+        <meta charser="utf-8">
+        <link rel="stylesheet" type="text/css" href="theme.css">
+    </head>
+
+    <body>
+        <h1>Questions List</h1>
+        <br><a href=""><img src="addicon.png" width=20px height=20px>Add a new question...</a></br>
+        <br></br>
+        <table>
+            <tr>
+                <th class="col1"> Question ID </th>
+                <th class="col2"> Question </th>
+                <th class="col3"> Content </th>
+                <th class="col4"> Action </th>
+            </tr>
+
+            <tr>
+                <td class="col1">q01</td>
+                <td class="col2">01</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q02</td>
+                <td class="col2">02</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q03</td>
+                <td class="col2">03</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q04</td>
+                <td class="col2">04</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q05</td>
+                <td class="col2">05</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q06</td>
+                <td class="col2">06</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q07</td>
+                <td class="col2">07</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q08</td>
+                <td class="col2">08</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q09</td>
+                <td class="col2">09</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q10</td>
+                <td class="col2">10</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q11</td>
+                <td class="col2">11</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q12</td>
+                <td class="col2">12</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q13</td>
+                <td class="col2">13</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q14</td>
+                <td class="col2">14</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q15</td>
+                <td class="col2">15</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q16</td>
+                <td class="col2">16</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q17</td>
+                <td class="col2">17</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q18</td>
+                <td class="col2">18</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q19</td>
+                <td class="col2">19</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q20</td>
+                <td class="col2">20</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q21</td>
+                <td class="col2">21</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q22</td>
+                <td class="col2">22</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q23</td>
+                <td class="col2">23</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q24</td>
+                <td class="col2">24</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q25</td>
+                <td class="col2">25</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q26</td>
+                <td class="col2">26</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q27</td>
+                <td class="col2">27</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q28</td>
+                <td class="col2">28</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q29</td>
+                <td class="col2">29</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q30</td>
+                <td class="col2">30</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q31</td>
+                <td class="col2">31</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q32</td>
+                <td class="col2">32</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q33</td>
+                <td class="col2">33</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q34</td>
+                <td class="col2">34</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q35</td>
+                <td class="col2">35</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q36</td>
+                <td class="col2">36</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q37</td>
+                <td class="col2">37</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q38</td>
+                <td class="col2">38</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q39</td>
+                <td class="col2">39</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q40</td>
+                <td class="col2">40</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q41</td>
+                <td class="col2">41</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q42</td>
+                <td class="col2">42</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q43</td>
+                <td class="col2">43</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q44</td>
+                <td class="col2">44</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q45</td>
+                <td class="col2">45</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q46</td>
+                <td class="col2">46</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q47</td>
+                <td class="col2">47</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q48</td>
+                <td class="col2">48</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q49</td>
+                <td class="col2">49</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+            <tr>
+                <td class="col1">q50</td>
+                <td class="col2">50</td>
+                <td class="col3">What is your favorite food?</td>
+                <td class="col4"><a class='act' href='#'>Edit</a>/<a class='act' href='#'>Delete</a></td> 
+            </tr>
+
+        </table>
+    </body>
+</html>
